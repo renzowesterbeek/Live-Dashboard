5ef1f55b35cb5986115fce6c7262c7aac7ae3db2
Renzo Westerbeek <renzowesterbeek@gmail.com>
2015-09-05 09:10:58 +0200

    Add time and date to page

--- js/datetime.js
+++ js/datetime.js
@@ -1,12 +1,35 @@
+function returnDayName(date){
+  var daynum = date.getDate();
+  if(daynum === 0){
+    return "Monday";
+  } else if (daynum == 1){
+    return "Tuesday";
+  } else if (daynum == 2){
+    return "Wednesday";
+  } else if (daynum == 3){
+    return "Thursday";
+  } else if (daynum == 4){
+    return "Friday";
+  } else if (daynum == 5){
+    return "Saturday";
+  } else if (daynum == 6){
+    return "Sunday";
+  }
+}
+
 function returnTime(){
   var date = new Date();
   var hours = date.getHours();
   var minutes = date.getMinutes();
-  return hours + ":" + minutes;
+  return zerofix(hours) + ":" + zerofix(minutes);
 }
 
 function returnDate(){
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   var date = new Date();
-  var day = date.getDay();
-  return day;
+  var weekday = returnDayName(date);
+  var day = date.getDate();
+  var month = monthNames[date.getMonth()];
+
+  return weekday + " " + day + " " + month;
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  // Call everything
+  $('#clock h1').html(returnTime);
+  $('#clock #date').html(returnDate);
+
+  setInterval(function(){
+    $('#clock h1').html(returnTime);
+    $('#clock #date').html(returnDate);
+  }, 10000);
 });
