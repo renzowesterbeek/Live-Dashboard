c6c3898a83ed2d7f519e61b6ec3cd00b662c04e4
Renzo Westerbeek <renzowesterbeek@gmail.com>
2015-09-30 12:20:52 +0200

    New update script

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 This is my personal dashboard displaying live information. Made for a screen with a 1080p resolution.
 
 ## Todo
+- [ ] Todo-list
+- [ ] Website auto-updating
 - [x] Clock
 - [x] Weather (today and tomorrow)
-- [ ] Todo-list
 - [x] Email
 - [x] Calendar
 - [x] Live updating
--- update.js
+++ update.js
@@ -1,52 +1,25 @@
+var fs = require('fs');
 var http = require('http');
 var request = require('request');
-var fs = require('fs');
 
-var getDownloadlink = function(callback){
-  var options = {
-    url: 'https://api.github.com/repos/renzowesterbeek/drinkyowater/releases',
-    headers: {
-      'User-Agent': 'request',
-    }
+var localfile = "";
+var remotefile = "";
+
+fs.readFile('package.json', 'utf8', function (err, data) {
+  if(!err){
+    localfile = data; // get local file
+  } else {
+    throw err;
   }
 
-  request(options, function (error, response, body) {
-    if(error){
-      console.log("ERROR OCCURED " + error);
-      return 0;
-    }
-    if (!error && response.statusCode == 200) {
-      var body = JSON.parse(body);
-      callback(body.zipball_url);
-      return 1;
-    }
-  });
-}
-
-setInterval(function(){
-  console.log('Checking for updates...');
-  // Check for package updates
-  var currentFile = "";
-  var remoteFile = "";
-
-  fs.readFile('version.md', 'utf-8', function (err, data) {
-    if (err) throw err;
-    currentFile = data;
-  });
-
-  request('http://raw.githubusercontent.com/renzowesterbeek/Live-Dashboard/master/version.md', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      remoteFile = body;
-      if(currentFile !== remoteFile){
-        console.log('Update available');
-        getDownloadlink(function(url){
-          console.log(url);
-          var file = fs.createWriteStream("file.zip");
-          var request = http.get(url, function(response) {
-            response.pipe(file);
-          });
-        });
+  request('https://rawgit.com/renzowesterbeek/siteupdater/master/package.json', function(error, response, body) {
+    if(!error && response.statusCode == 200){
+      remotefile = body; // get remote file
+      if(localfile !== remotefile){
+        console.log("there was a difference");
       }
+    } else {
+      throw error;
     }
   });
-}, 2*1000); // Every 2 seconds
+});
