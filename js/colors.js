1ac09bed0ec786a7564011e9b7b8893443def148
Renzo Westerbeek <renzowesterbeek@gmail.com>
2016-02-29 10:12:02 +0100

    Fix colors

--- js/colors.js
+++ js/colors.js
@@ -1,19 +1,19 @@
 function colors(){
   $.get( "http://api.sunrise-sunset.org/json?lat=52.379189&lng=4.899431&formatted=0", function( data ) {
-    var sunrise = new Date(data.results.sunrise);
-    var sunset = new Date(data.results.sunset);
+    var sunriseTime = new Date(data.results.sunrise).getTime();
+    var sunsetTime = new Date(data.results.sunset).getTime();
+
+    var curTime = new Date().getTime();
+
+    if(curTime < sunsetTime && curTime > sunriseTime){
+      // Day
+      $("body").css("background-color","#3498DB");
+    } else {
+      // Night
+      $("body").css("background-color","#34495D");
+      $("body").css("color","#ECF0F1");
+    }
+
   });
-
-  var curTime = new Date();
-
-
-  if(curTime < sunset && curTime > sunrise){
-    // Day
-    $("body").css("background-color","#3498DB");
-  } else {
-    // Night
-    $("body").css("background-color","#34495D");
-    $("body").css("color","#ECF0F1");
-  }
 
 }
